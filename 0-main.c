d5c063616f5a6d79102475f0f0decd41ffbf6f85
mellanieachieng <mellanie.achieng@gmail.com>
2022-07-17 17:07:09 +0000

    bug fix

--- 0-main.c
+++ 0-main.c
@@ -9,17 +9,6 @@
 */
 int main(void)
 {
-<<<<<<< HEAD
-int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-size_t n = sizeof(array) / sizeof(array[0]);
-
-print_array(array, n);
-printf("\n");
-bubble_sort(array, n);
-printf("\n");
-print_array(array, n);
-return (0);
-=======
 	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
@@ -29,5 +18,4 @@ return (0);
 	printf("\n");
 	print_array(array, n);
 	return (0);
->>>>>>> 24a9282348b433f6dbae4b9c0a7cba0029fd474b
 }
--- print_array.c
+++ print_array.c
@@ -9,19 +9,6 @@
 */
 void print_array(const int *array, size_t size)
 {
-<<<<<<< HEAD
-size_t i;
-
-i = 0;
-while (array && i < size)
-{
-if (i > 0)
-printf(", ");
-printf("%d", array[i]);
-++i;
-}
-printf("\n");
-=======
 	size_t i;
 
 	i = 0;
@@ -33,5 +20,4 @@ printf("\n");
 	++i;
 	}
 	 printf("\n");
->>>>>>> 24a9282348b433f6dbae4b9c0a7cba0029fd474b
 }
